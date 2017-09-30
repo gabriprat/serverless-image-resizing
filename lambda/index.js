148283651e5c14ef3c86c9fb255e4fdcc851a510
Gabriel Prat Masramon <gabriprat@gmail.com>
2017-09-30 09:52:16 +0200

    Random query strings to prevent caching

--- lambda/index.js
+++ lambda/index.js
@@ -8,17 +8,23 @@ const Sharp = require('sharp');
 
 const BUCKET = process.env.BUCKET;
 const URL = process.env.URL;
+const REDIRECT_TO = process.env.REDIRECT_TO;
 
 exports.handler = function(event, context, callback) {
   const key = event.queryStringParameters.key;
-  const match = key.match(/(\d+)x(\d+)\/(.*)/);
+  const match = key.match(/(\d+)x(\d+)\/([\w\.]*).*/);
   const width = parseInt(match[1], 10);
   const height = parseInt(match[2], 10);
   const originalKey = match[3];
-
+  const qs = `?r=${Math.random()}`;
+  
+  console.log(`KEY: ${key}`);
+  console.log(`ORIGINAL KEY: ${originalKey}`);
+  console.log(`QS: ${qs}`);
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
     .then(data => Sharp(data.Body)
       .resize(width, height)
+      .max()
       .toFormat('png')
       .toBuffer()
     )
@@ -26,12 +32,12 @@ exports.handler = function(event, context, callback) {
         Body: buffer,
         Bucket: BUCKET,
         ContentType: 'image/png',
-        Key: key,
+        Key: `${width}x${height}/${originalKey}`,
       }).promise()
     )
     .then(() => callback(null, {
         statusCode: '301',
-        headers: {'location': `${URL}/${key}`},
+        headers: {'location': `${REDIRECT_TO}/${originalKey}${qs}`},
         body: '',
       })
     )
